11106c918a11ed9267f989301c9a479cb2e6c666
Michael Canlas <michael.canlas428@gmail.com>
2014-10-13 17:36:58 -0600

    made an array of objects

--- csv-to-json.js
+++ csv-to-json.js
@@ -2,10 +2,14 @@ var fs = require('fs');
 
 
 var sourceFileName = process.argv[2];
+var destinationFileName = process.argv[3];
 var inputContent = fs.readFileSync(sourceFileName, 'utf-8');
 
 var array = [];
 var keys = [];
+var obj0 = {};
+var obj1 = {};
+var obj2 = {};
 var content = array.push(inputContent);
 
 var arrayValues = array.toString().split('\n');
@@ -16,10 +20,11 @@ var key = keys.toString().split(',');
 console.log('values: ', values);
 console.log('--------');
 
+var test = values.map(function(item){
+	console.log(key['name'] = item.split(',')[0], key['age'] = item.split(',')[1], key['city'] = item.split(',')[2] );
 
-for(var i = 0; i < values.length; i++){
-	console.log(values[i]);
-}
+});
+
 console.log('--------');
 var value = values.toString().split(',');
 
@@ -41,11 +46,25 @@ for(var o = 2; o < value.length; o = o+3){
 	city.push(value[o]);
 }
 
-console.log('keys: ', key);
-console.log('name: ', name);
-console.log('age: ', age);
-console.log('city: ', city);
-
 console.log('--------');
 // use map
+obj0[keys.toString().split(',')[0]] = name[0];
+obj0[keys.toString().split(',')[1]] = age[0];
+obj0[keys.toString().split(',')[2]] = city[0];
+
+obj1[keys.toString().split(',')[0]] = name[1];
+obj1[keys.toString().split(',')[1]] = age[1];
+obj1[keys.toString().split(',')[2]] = city[1];
+
+obj2[keys.toString().split(',')[0]] = name[2];
+obj2[keys.toString().split(',')[1]] = age[2];
+obj2[keys.toString().split(',')[2]] = city[2];
+
+var newObject = [obj0,obj1,obj2]
+console.log(newObject);
+
+console.log('--------');
 console.log(inputContent);
+
+// writing into output.js
+fs.writeFileSync(destinationFileName, newObject);
